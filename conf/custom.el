6523a7fb64336e1a6eb9dcea5c93c6afde84c410
Vitor Arins <vitorarins@gmail.com>
2015-09-06 01:35:46 -0300

    Added go compilation

--- conf/custom.el
+++ conf/custom.el
@@ -37,6 +37,10 @@
 (defun my-go-mode-hook ()
   ; Call Gofmt before saving                                                    
   (add-hook 'before-save-hook 'gofmt-before-save)
+  ; Customize compile command to run go build
+  (if (not (string-match "go" compile-command))
+      (set (make-local-variable 'compile-command)
+           "go build -a -v && go test -v && go vet"))
   ; Godef jump key binding                                                      
   (local-set-key (kbd "<C-return>") 'godef-jump))
 (add-hook 'go-mode-hook 'my-go-mode-hook)
